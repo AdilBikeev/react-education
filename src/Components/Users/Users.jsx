6348280129e6f37431838e1b0ca9fd1e9506d809
AdilBikeev <bikeev1999@mail.ru>
2020-05-13 03:04:19 +0300

    Переделал Users в классовую компоненту

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -2,28 +2,29 @@
 import s from './Users.module.css';
 import * as axios from 'axios';
 
-export const Users = (props) => {
-
-    if(props.users.length === 0)
+export class Users extends React.Component {
+    constructor(props)
     {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-             .then(response => {
-                props.setUsers(response.data.items); 
-             });
+        super(props);
+        if(props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                 .then(response => {
+                    this.props.setUsers(response.data.items);
+                 });
+        }
     }
-
-    return (
-        <div>
+    render(){
+        return <div>
             {
-                props.users.map(x => <div key={x.id}>
+                this.props.users.map(x => <div key={x.id}>
                     <span>
                         <div>
                             <img alt='images' className={s.userPhoto} src={x.photos.small != null ? x.photos.small : 'https://avatars.mds.yandex.net/get-pdb/1920690/8846c29f-cdf4-4249-a1d0-83cef96661d0/s1200' } />
                         </div>
                         <div>
                             {x.followed === true ?
-                                <button onClick={() => { props.unfollow(x.id) }}>Follow</button>
-                                : <button onClick={() => { props.follow(x.id) }}>unFollow</button>}
+                                <button onClick={() => { this.props.unfollow(x.id) }}>Follow</button>
+                                : <button onClick={() => { this.props.follow(x.id) }}>unFollow</button>}
                         </div>
                     </span>
                     <span>
@@ -39,5 +40,5 @@ export const Users = (props) => {
                 </div>)
             }
         </div>
-    );
+    }
 };
