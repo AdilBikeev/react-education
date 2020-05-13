6794f2a8fe74b4cd31134eea8d5ddad41309b8ca
AdilBikeev <bikeev1999@mail.ru>
2020-05-13 08:35:09 +0300

    Add componentDidMount for Users component

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -6,7 +6,9 @@ export class Users extends React.Component {
     constructor(props)
     {
         super(props);
-        if(props.users.length === 0) {
+    }
+    componentDidMount(){
+        if(this.props.users.length === 0) {
             axios.get("https://social-network.samuraijs.com/api/1.0/users")
                  .then(response => {
                     this.props.setUsers(response.data.items);
