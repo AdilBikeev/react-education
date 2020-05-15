c097500274713ff2a5f2924bc75958034cfceef5
AdilBikeev <bikeev1999@mail.ru>
2020-05-15 20:03:21 +0300

    Add possibility change status dynamic

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import { ProfileStatusContainer } from './ProfileStatus/ProfileStatus';
 
 const Profile = (props) => {
     return (
         <div>
             <ProfileInfo profile={props.profile} />
+            <ProfileStatusContainer />
             <MyPostsContainer />
         </div>
     )
